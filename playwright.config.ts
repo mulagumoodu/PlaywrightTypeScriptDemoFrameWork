6442c66a872ff140a2f7ea5f7ea4549e2e469066
benti <abenticruz@gmail.com>
2026-03-02 17:56:50 -0800

    Update Playwright config to use Allure reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
   ['list'],
-  ['allure-playwright']
+  ['allure-playwright'],
+  ['html']
 ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
